2d67e9b57ca7f60e98dafac9d623fcaaf659b054
Bill G <gbilly3@gmail.com>
2019-11-12 00:46:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,10 @@
 function findMatching (drivers,string) {
   return drivers.filter(driver=> string.toLowerCase() === driver.toLowerCase())
 }
+
+
+function findMatching (drivers, string ) {
+
+  return drivers.filter(driver => string.charAt(0) === driver.charAt(0))
+  
+}
