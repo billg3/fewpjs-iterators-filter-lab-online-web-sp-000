a91721e639cd95dd79188d20ddf99d80bc1efd8f
Bill G <gbilly3@gmail.com>
2019-11-12 00:27:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 // Code your solution here
 
-function findMatching (list, name) {
-  return list.filter(function (driverName) {
-    return driverName.toLowerCase() === name.toLowerCase();
-  });
+function findMatching (drivers,string) {
+  return drivers.filter(driver=> string.toLowerCase() === driver.toLowerCase())
 }
